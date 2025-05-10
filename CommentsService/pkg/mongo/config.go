81547694d8f220948e560fc1652675128c513b8b
zombiehunter <viktormallory25@gmail.com>
2025-05-10 16:09:45 +0300

    feat(Comments/mongo): get config parameters from environment variables

--- CommentsService/pkg/mongo/config.go
+++ CommentsService/pkg/mongo/config.go
@@ -2,14 +2,35 @@ package mongo
 
 import (
 	"fmt"
+	"os"
 
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var ErrConfParamMissing = fmt.Errorf("configuration parameter missing")
 
 type Config struct {
 	Host   string
 	Port   string
 	DBName string
+}
+
+func NewConfig() (*Config, error) {
+	conf := new(Config)
+	conf.Host = os.Getenv("MONGO_HOST")
+	if conf.Host == "" {
+		return nil, fmt.Errorf("%w: MONGO_HOST", ErrConfParamMissing)
+	}
+	conf.Port = os.Getenv("MONGO_PORT")
+	if conf.Port == "" {
+		return nil, fmt.Errorf("%w: MONGO_PORT", ErrConfParamMissing)
+	}
+	conf.DBName = os.Getenv("MONGO_DB_NAME")
+	if conf.DBName == "" {
+		return nil, fmt.Errorf("%w: MONGO_DB_NAME", ErrConfParamMissing)
+	}
+
+	return conf, nil
 }
 
 func (c *Config) conString() string {
