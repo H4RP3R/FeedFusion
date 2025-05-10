927b9eea86c04832d73849801d738518bbb2c95f
zombiehunter <viktormallory25@gmail.com>
2025-05-10 18:01:42 +0300

    feat(Gateway/server): add graceful shutdown

--- APIGateway/cmd/server/main.go
+++ APIGateway/cmd/server/main.go
@@ -1,16 +1,47 @@
 package main
 
 import (
-	"log"
+	"context"
+	"errors"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	log "github.com/sirupsen/logrus"
 
 	"gateway/pkg/api"
 )
 
+const serverPort = ":8088"
+
 func main() {
 	api := api.New()
-	err := http.ListenAndServe(":8080", api.Router())
-	if err != nil {
-		log.Fatal(err)
+
+	srv := &http.Server{
+		Addr:    serverPort,
+		Handler: api.Router(),
+	}
+
+	go func() {
+		log.Infof("[server] starting on port %v", serverPort)
+		if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Errorf("[server] failed to start: %v", err)
+			return
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownRelease()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Errorf("[server] HTTP server shutdown error: %v", err)
+	} else {
+		log.Info("[server] HTTP server shut down gracefully")
 	}
 }
