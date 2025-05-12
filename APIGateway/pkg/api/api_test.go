874da873989f81d8ca8e0e77b40f8d8a28f070a5
zombiehunter <viktormallory25@gmail.com>
2025-05-12 21:14:45 +0300

    test(Gateway/api): add test for filterNewsProxy

--- APIGateway/pkg/api/api_test.go
+++ APIGateway/pkg/api/api_test.go
@@ -3,15 +3,17 @@ package api
 import (
 	"bytes"
 	"encoding/json"
-	"gateway/pkg/models"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 	"time"
 
 	"github.com/gofrs/uuid"
 	"github.com/h2non/gock"
+
+	"gateway/pkg/models"
 )
 
 func TestAPI_createCommentProxy(t *testing.T) {
@@ -77,3 +79,42 @@ func TestAPI_createCommentProxy(t *testing.T) {
 }
 
 // TODO: add negative scenario test for createCommentProxy.
+
+func TestAPI_filterNewsProxy(t *testing.T) {
+	defer gock.Off()
+
+	responseNews := []models.Post{
+		{
+			ID:        uuid.FromStringOrNil("f3767624-65e9-5e26-80e1-aea970710389"),
+			Title:     "The Rise of AI Code Assistants: Revolutionizing Software Development",
+			Content:   `The world of software development is undergoing a significant transformation with the emergence of AI code assistants. These intelligent tools are designed to assist developers in writing, debugging, and optimizing their code, making the development process faster, more efficient, and more enjoyable.`,
+			Published: time.Date(2024, 12, 2, 0, 0, 0, 0, time.UTC),
+			Link:      "https://tech/posts/1198",
+		},
+	}
+
+	api := New()
+
+	gock.New(newsServiceURL).Reply(http.StatusOK).JSON(responseNews)
+
+	req := httptest.NewRequest(http.MethodGet, "/news/filter?contains=AI", nil)
+	rr := httptest.NewRecorder()
+	api.Router().ServeHTTP(rr, req)
+	if rr.Code != http.StatusOK {
+		t.Errorf("want status code %v, got status code %v", http.StatusOK, rr.Code)
+	}
+
+	var gotNews []models.Post
+	b, err := io.ReadAll(rr.Body)
+	if err != nil {
+		t.Fatalf("failed to read response body: %v", err)
+	}
+	err = json.Unmarshal(b, &gotNews)
+	if err != nil {
+		t.Errorf("failed to unmarshal response body: %v", err)
+	}
+
+	if !reflect.DeepEqual(responseNews, gotNews) {
+		t.Errorf("want response news\n%+v\n\ngot response news\n%+v\n", responseNews, gotNews)
+	}
+}
