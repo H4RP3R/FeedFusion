5553b6cd3b7913285795de7d6cab080003f59252
zombiehunter <viktormallory25@gmail.com>
2025-05-15 14:16:04 +0300

    refactor(Comments/server): replace hardcoded server params with flags

--- CommentsService/cmd/server/main.go
+++ CommentsService/cmd/server/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"errors"
+	"flag"
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 
@@ -15,16 +17,40 @@ import (
 	"comments/pkg/mongo"
 )
 
-const serverPort = ":8077"
-
 func main() {
+	var (
+		httpAddr string
+		logLevel string
+	)
+
+	flag.StringVar(&httpAddr, "http", ":8077", "HTTP server address in the form 'host:port'.")
+	flag.StringVar(&logLevel, "log", "info", "Log level: debug, info, warn, error.")
+	flag.Parse()
+
+	if !strings.Contains(httpAddr, ":") {
+		log.Warn("use ':' before port number, e.g. ':8080'")
+	}
+
+	switch logLevel {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	}
+
 	conf, err := mongo.NewConfig()
 	if err != nil {
 		log.Errorf("[server] failed to connect to Mongo: %v", err)
 		return
 	}
 
-	db, err := mongo.New(conf)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	db, err := mongo.New(ctx, conf)
 	if err != nil {
 		log.Errorf("[server] failed to initialize storage instance, DB connection not established: %v", err)
 		return
@@ -32,15 +58,14 @@ func main() {
 
 	api := api.New(db)
 	srv := &http.Server{
-		Addr:    serverPort,
+		Addr:    httpAddr,
 		Handler: api.Router(),
 	}
 
 	go func() {
-		log.Infof("[server] starting on port %v", serverPort)
+		log.Infof("[server] starting on port %v", httpAddr)
 		if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
-			log.Errorf("[server] failed to start: %v", err)
-			return
+			log.Fatalf("[server] failed to start: %v", err)
 		}
 	}()
 
