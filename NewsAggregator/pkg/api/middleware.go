0c84f64d82123b7434ddf9ed2e96d473d6f1cd3a
zombiehunter <viktormallory25@gmail.com>
2025-05-16 18:32:29 +0300

    feat(Aggregator/api): add requestIDMiddleware

--- NewsAggregator/pkg/api/middleware.go
+++ NewsAggregator/pkg/api/middleware.go
@@ -1,6 +1,29 @@
 package api
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type ctxKeyRequestID struct{}
+
+var RequestIDKey = ctxKeyRequestID{}
+
+func (api *API) requestIDMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		reqID := r.Header.Get("X-Request-Id")
+		if reqID == "" {
+			log.Warnf("[requestIDMiddleware] missing X-Request-Id header from %v", r.RemoteAddr)
+			http.Error(w, "Missing X-Request-Id header", http.StatusBadRequest)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), RequestIDKey, reqID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
 
 func (api *API) headerMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
