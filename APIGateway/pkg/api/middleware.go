3f10f09e162b47740a5865ad607c78d3b6df959e
zombiehunter <viktormallory25@gmail.com>
2025-05-16 18:27:48 +0300

    feat(Gateway/api): add requestIDMiddleware

--- APIGateway/pkg/api/middleware.go
+++ APIGateway/pkg/api/middleware.go
@@ -1,6 +1,36 @@
 package api
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+
+	"github.com/gofrs/uuid"
+	log "github.com/sirupsen/logrus"
+)
+
+type ctxKeyRequestID struct{}
+
+var RequestIDKey = ctxKeyRequestID{}
+
+func (api *API) requestIDMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		reqID := r.Header.Get("X-Request-Id")
+		if reqID == "" {
+			id, err := uuid.NewV4()
+			if err != nil {
+				log.Errorf("[requestIDMiddleware] failed to generate request ID for %v: %v", r.RemoteAddr, err)
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+				return
+			}
+			reqID = id.String()
+			log.Debugf("[requestIDMiddleware] generated request ID:%s for %v", reqID, r.RemoteAddr)
+		}
+
+		w.Header().Set("X-Request-Id", reqID)
+		ctx := context.WithValue(r.Context(), RequestIDKey, reqID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
 
 func (api *API) headerMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
